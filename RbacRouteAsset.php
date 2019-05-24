47751dd7af96fd5a18e463165bc3711e1a86c1c7
Dencho Tsanov <tsanov.dencho@gmail.com>
2019-05-24 09:18:01 +0300

    rbac

---
+++ RbacRouteAsset.php
@@ -0,0 +1,12 @@
+<?php
+namespace denchotsanov\yii2rbac;
+
+class RbacRouteAsset extends RbacAsset
+{
+    /**
+     * @var array
+     */
+    public $js = [
+        'js/rbac-route.js',
+    ];
+}
